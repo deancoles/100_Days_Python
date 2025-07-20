d8eb9ca055310c0d690b6852e9a88fa3e9d05fd6
Dean Coles <dean_coles@outlook.com>
2025-07-20 19:55:12 +0100

    Day 01
    - All Day 01 exercises and project completed.
    - Added resource files.

---
+++ Exercises/Day 01/02. Debugging Practice.py
@@ -0,0 +1,15 @@
+# There are errors on all 5 lines of code. Fix the code so that it runs without errors.
+
+# Original
+print(Notes from Day 1")
+ print("The print statement is used to output strings")
+print("Strings are strings of characters"
+priint("String Concatenation is done with the + sign")
+print(("New lines can be created with a \ and the letter n")
+
+# Fixed
+print("Notes from Day 1")
+print("The print statement is used to output strings")
+print("Strings are strings of characters")
+print("String Concatenation is done with the + sign")
+print("New lines can be created with a \ and the letter n")
---
+++ Projects/01. Band Name Generator.py
@@ -0,0 +1,11 @@
+# Create a program that generates a band name for the user
+
+# Greeting
+print("Welcome to the Band Name Generator")
+
+# User inputs city and pet
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's the name of your first pet?\n")
+
+# Program returns band name
+print(f"Your band name could be {city} {pet}" )
